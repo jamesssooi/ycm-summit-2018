5048389d2dea8327bee35b6a3f369af8c943a54e
James Ooi <wengteikooi@gmail.com>
2018-09-11 21:09:25 +0800

    Implement basic footer

--- src/index.php
+++ src/index.php
@@ -373,6 +373,51 @@
 </section>
 
 
+<!-- Footer -->
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+
+      <!-- Left Column -->
+      <div class="col-8">
+        <a href="#" class="footer__logo mb-4">
+          <img src="img/logos/ycm-white.svg" alt="Logo of YCM">
+        </a>
+        <small class="footer__disclaimer">Copyright (C) 2018 Young Corporate Malaysians</small>
+      </div>
+
+      <!-- Right Column -->
+      <div class="col-4">
+        <div class="row">
+
+          <!-- Links  -->
+          <div class="col-6">
+            <ul class="footer-links">
+              <li><a href="#">About</a></li>
+              <li><a href="#">Agenda</a></li>
+              <li><a href="#">Speakers</a></li>
+              <li><a href="#">Sponsors</a></li>
+              <li><a href="#">Register</a></li>
+              <li><a href="#">About YCM</a></li>
+            </ul>
+          </div>
+
+          <!-- Social Links -->
+          <div class="col-6">
+            <ul class="footer-links">
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Instagram</a></li>
+              <li><a href="#">LinkedIn</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
