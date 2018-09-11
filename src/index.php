28b5bdc7c31ecf7e59879f5692b6ac227aa9d400
James Ooi <wengteikooi@gmail.com>
2018-09-11 18:33:07 +0800

    Implement agenda listing section

--- src/index.php
+++ src/index.php
@@ -184,6 +184,43 @@
   </div>
 </section>
 
+
+<!-- Agenda Section -->
+<section class="agenda-section">
+  <div class="container">
+    <div class="agenda-container">
+
+      <!-- Header -->
+      <header class="section-header">
+        <h2 class="section-title">Agenda</h2>
+      </header>
+
+      <!-- Agenda Listings -->
+      <ul class="agenda-item-list row">
+        <?php for ($i = 0; $i < 8; $i++): ?>
+        <li class="col-6">
+          <button class="agenda-item">
+            <div class="agenda-item__header">
+              <span class="agenda-item__type">Panel Discussion</span>
+              <p class="agenda-item__title">
+                Distributed economy in context: An economic, legal, corporate,
+                and global perspective
+              </p>
+            </div>
+            <div class="agenda-item__icon">
+              <svg class="icon">
+                <use xlink:href="svg/sprites.svg#icon-arrow-right"></use>
+              </svg>
+            </div>
+          </button>
+        </li>
+        <?php endfor; ?>
+      </ul>
+
+    </div>
+  </div>
+</section>
+
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
