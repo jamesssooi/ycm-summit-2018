89487e9bb61414b0a342ff4e37e6f1a71ba444e0
James Ooi <wengteikooi@gmail.com>
2018-09-17 12:27:01 +0800

    Add favicons

--- src/index.php
+++ src/index.php
@@ -14,6 +14,9 @@ include 'data/agenda.php';
 
   <!-- SEO & Meta -->
   <title>Distributed Economy • YCM 10th Annual Summit</title>
+  <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16" />
+
 
   <!-- Stylesheets -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
