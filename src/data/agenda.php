4d732db55267ba8bf292186ab3a353fcd28b5320
James Ooi <wengteikooi@gmail.com>
2018-09-23 22:01:04 +0800

    Update agenda

--- src/data/agenda.php
+++ src/data/agenda.php
@@ -3,37 +3,37 @@
 $agenda = [
   [
     'type' => 'Keynote',
-    'title' => 'Race against the machine',
-    'description' => '<p>Macro view of technology now and future, how the distributed economy came about, and how it will impact us (society/country).</p>'
+    'title' => 'A race against the machine',
+    'description' => '<p>An introductory session to the theme highlighting how digitalization will accelerate innovation, drive productivity and bring about inevitable disruptions that will irreversibly transform employment and the economy.</p>'
   ],
   [
     'type' => 'Flash Talk',
     'title' => 'An evolution of economies: From centralised, shared, to distributed',
-    'description' => '<p>Are economies naturally path dependent to evolve from centralised to distributed? This session aims to provide a general overview on the history of economies, and to look at history for inspiration for the economies of the future. It also gives an in-depth understanding of the basics and concepts that brings about a distributed economy.</p>'
+    'description' => '<p>Economics holds a central position in any international relations issues. With the support and enablement of technologies, are economies naturally path dependent to evolve from centralised to distributed? This session aims to provide a general overview on the history of economies, and give an in-depth understanding of the concept and fundamentals of a distributed economy.</p>'
   ],
   [
     'type' => 'Panel Discussion',
     'title' => 'Distributed economy in context: An economic, legal, regulatory, corporate, and global perspective',
-    'description' => '<p>The world of distributed economies will force changes in the legal, corporate, and governance spheres. Delegates will be able to examine how will these forces come about, explore the world of Distributed Economy and delve into various perspectives on the changes that DE brings to the table in this panel. With the ever changing needs of the society, we will discuss the necessary policies and social protections needed to ensure a seamless transition into distributed economy.<p>'
+    'description' => '<p>The world of distributed economies will force changes in the legal, corporate, and governance spheres.</p><p>This session aims to explore how will inclusive peer-to-peer and permissionless technologies potentially shift our social paradigm? How our economy will evolve through open financial technologies? We will discuss the necessary policies, regulations and  social protection required to ensure a seamless transition into a distributed economy from various perspectives.<p>'
   ],
   [
     'type' => 'Flash Talk',
-    'title' => 'Technology and inequality',
-    'description' => '<p>In this session, we will discuss the disadvantages the rise of robust technology could possibly bring, especially to the poor communities.</p>'
+    'title' => 'An economy that works for everyone',
+    'description' => '<p>In this session, we will discuss if structuring an economy as a distributed network can more equitably distribute income and wealth amongst all those who help to generate it. This session pushes delegates to rethink many conventional economic theories such as Money creation & resource ownership: why endow commercial banks with the right to create money as interest-based debt? Could it be alternatively created by the state, or communities? What is the the potential of free open-source hardware and software licensing - open access to information? How could it instrumental in reducing inequality?</p>'
   ],
   [
     'type' => 'Panel Discussion',
-    'title' => 'What is blockchain?',
-    'description' => '<p>Understanding what blockchain technology is and how it could potentially bring about a distributed economy. Focused on examples of how the tehnology is changing/ could change our daily lives and the way we do business.</p><h5><strong>Blockchain: The Game Changer</strong></h5><p>Where will the biggest impact of Blockchain be? Which industries are leveraging the potential of Blockchain?</p><p>This session opens insights into the development of new applications and services based on increased privacy, security, efficiency and transparency that will fundamentally change the way governments and  enterprise do business by highlighting key use cases of blockchain in SEA and around the globe.</p>'
+    'title' => 'Blockchain: The World’s Game Changer?',
+    'description' => '<p>What is blockchain — and how will it enable a more distributed economy? Where will the biggest impact of Blockchain be? Which industries are leveraging the potential of Blockchain?</p><p>This session opens insights into the development of new applications and services based on increased privacy, security, efficiency and transparency that could fundamentally disrupt governments and enterprise (MNCs & SMEs) operations by highlighting key examples of blockchain in SEA and around the globe.</p>'
   ],
   [
     'type' => 'Panel Discussion',
     'title' => 'Distributed economy: A sustainable engine for innovation?',
-    'description' => '<p>Is Distributed Economy a fresh strategy to guide sustainable industrial development? This session brings together thought leaders from various backgrounds (Corporate, goverment, startup and legal) to paint a vision of possible changes, technologies, opportunities, threats, risks and concerns that comes with a distributed economy. This session is also expected to educate young professionals how to better prepare for what\'s coming - How can we harness opportunities? How to reduce or avoid threats?</p>'
+    'description' => '<p>Is Distributed Economy a fresh strategy to guide sustainable industrial development? This session brings together thought leaders from various backgrounds (Corporate, government, start-up and legal) to paint a vision of possible changes, technologies, opportunities, threats, risks and concerns related to employment and job creation that comes with a distributed economy. This session is also expected to educate young professionals how to better prepare for what\'s coming - How can we harness opportunities? How do we reduce or avoid threats?</p>'
   ],
   [
     'type' => 'Keynote',
-    'title' => 'Distributed economy and our future',
-    'description' => ''
+    'title' => 'What’s next for young professionals?',
+    'description' => '<p>A session that highlights key action steps that young professionals can take to prepare themselves to thrive in a distributed economy.</p>'
   ],
 ];
